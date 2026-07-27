90914a6be6f1719036b2f77b233e79fe3d1c48b1
oxtros.dev <43768428+thachvn0296@users.noreply.github.com>
2026-07-27 01:01:53 +0000

    Add comments for home route

--- bot.js
+++ bot.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 10000
 
 require('dotenv').config();
 
+
+//Home route to serve the index.html file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 })
